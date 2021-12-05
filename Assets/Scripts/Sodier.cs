2c22c77077d14fc2c845ae03cbeeaec17e9a44fc
HungSewingMachine <hung.vutrung19@gmail.com>
2021-12-05 21:30:04 +0700

    Fixed and add HorizontalConstrain
    Remove problem between Update and FixedUpdate when deal with Get Input and Physics, add some constrain to the left and right side

--- Assets/Scripts/Sodier.cs
+++ Assets/Scripts/Sodier.cs
@@ -5,41 +5,41 @@ using UnityEngine;
 public class Sodier : MonoBehaviour
 {
     Rigidbody _rb;
+    float movement;
+    float _xPosition;
+    Vector3 _position;
 
     [SerializeField]
-    float _thrust = 3f;
+    float _speed = 3f;
     [SerializeField]
-    float _side = 2f;
+    float _leftRight = 2f;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _xPosition = transform.position.x;
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        HorizontalConstrain();
     }
 
     void FixedUpdate()
     {
-        //_rb.velocity = transform.forward * _thrust;
-        //Move();
-        _rb.velocity = transform.forward * _thrust + new Vector3(Input.GetAxis("Horizontal") * _side, 0, 0);
+        _rb.velocity = transform.forward * _speed + new Vector3(movement * _leftRight, 0, 0);
     }
-    /*
-    void Move()
-    { 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (transform.position.x >= _leftBoundary)
-            {
-                transform.Translate(Vector3.left * _side * Time.deltaTime);
-            }
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (transform.position.x <= _rightBoundary)
-            {
-                transform.Translate(Vector3.right * _side * Time.deltaTime);
-            }
-        }
-    } */
 
-    
+    private void HorizontalConstrain()
+    {
+        if (transform.position.x < _xPosition - 4.85f)
+        {
+            transform.position = new Vector3(_xPosition- 4.85f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > _xPosition + 2.6f)
+        {
+            transform.position = new Vector3(_xPosition + 2.6f, transform.position.y, transform.position.z);
+        }
+    }
 }
